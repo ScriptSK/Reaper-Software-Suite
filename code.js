031d434183911932cb4976529eca4524b7e2f95f
ShininGami <31525262+ScriptSK@users.noreply.github.com>
2018-12-23 13:58:53 +0100

    Update code.js

--- code.js
+++ code.js
@@ -3,9 +3,9 @@ var app = angular.module("myApp", []);
 
 app.service("ProcessService", function ($http, $q)
 {
-	var url="file:///C:/Users/shini/Desktop";
+	var url="https://raw.githubusercontent.com/ScriptSK/ps4reaper.github.io/master/processlist.json";
 	var deferred = $q.defer();
-	$http.get("https://raw.githubusercontent.com/ScriptSK/ps4reaper.github.io/master/processlist.json").then(function (data)
+	$http.get("http://192.168.1.17:771/list").then(function (data)
 	{
 		deferred.resolve(data);
 	});
